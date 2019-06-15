3a0f1f9fb77d687dc101120969231d6247da6ccb
Matt Burke <spraints@gmail.com>
2019-06-15 15:44:28 -0400

    Set up an API

--- upornot/http.go
+++ upornot/http.go
@@ -2,16 +2,69 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func buildHTTPHandler(m *model) http.Handler {
-	return http.NewServeMux()
-	//mux.HandleFunc("/api/status", api.status)
-	// TODO - add a handler
-	// /api/status - {"status": "up|down"}
-	// /           - web page that renders the above.
+	mux := http.NewServeMux()
+	mux.Handle("/api/status", apiStatus(m))
+	// ? mux.HandleFunx("", homePage(m))
+	return mux
+}
+
+func apiStatus(m *model) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var res struct {
+			Count     int     `json:"count"`
+			AvgMillis float64 `json:"avg_ms"`
+			OKCount   int     `json:"ok"`
+			Buckets   [5]struct {
+				MaxMillis int `json:"max_ms,omitempty"`
+				Count     int `json:"count"`
+			} `json:"buckets"`
+		}
+		res.Buckets[0].MaxMillis = 60
+		res.Buckets[1].MaxMillis = 100
+		res.Buckets[2].MaxMillis = 200
+		res.Buckets[3].MaxMillis = 1000
+
+		vals := m.Get()
+		res.Count = len(vals)
+		var (
+			totalMs float64
+		)
+		for _, val := range vals {
+			if val.Result == ok {
+				res.OKCount++
+				ms := (val.Duration.Seconds() * 1000.0)
+				totalMs += ms
+				for i := 0; i < len(res.Buckets); i++ {
+					max := res.Buckets[i].MaxMillis
+					if max == 0 || ms < float64(max) {
+						res.Buckets[i].Count++
+						break
+					}
+				}
+			}
+		}
+		if res.OKCount > 0 {
+			res.AvgMillis = totalMs / float64(res.OKCount)
+		}
+
+		apiRes(w, res)
+	})
+}
+
+func apiRes(w http.ResponseWriter, res interface{}) {
+	if json, err := json.Marshal(res); err != nil {
+		logerr(err, "writing api response")
+		http.Error(w, "response marshaling failed", http.StatusInternalServerError)
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		w.Write(json)
+	}
 }
 
 func serveHTTP(ctx context.Context, server *http.Server) error {
